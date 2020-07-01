9f2b5218a985126f8d11191ddd40ad3401c8a68e
cgewecke <christophergewecke@gmail.com>
2020-07-01 12:56:45 -0700

    Update Web3 to 1.2.9 / use "stable" / update package-lock

--- test/test.sh
+++ test/test.sh
@@ -15,7 +15,7 @@ echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo "Instamining tests  (basic, http & websockets)"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
-node ./index.js --tag 'latest'
+node ./index.js --tag 'stable'
 npx mocha --grep "basic-http" --timeout 5000
 npx mocha --grep "basic-websockets" --timeout 5000 --exit
 cleanup
@@ -26,7 +26,7 @@ echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
 node ./index.js \
     --launch \
-    --tag 'latest' \
+    --tag 'stable' \
     --accounts 5 \
     --password 'hello' \
     --balance 50 \
@@ -40,5 +40,5 @@ echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo "Automining tests (--period)"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
-node ./index.js --tag 'latest' --period 1
+node ./index.js --tag 'stable' --period 1
 npx mocha --grep "period" --timeout 5000
